e3611fa84cb57820ce0298b374621c8d473b088d
arshiabyya <abhatt21@ucsc.edu>
2025-04-10 20:19:49 -0700

    Initial commit

---
+++ testPost.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Post = require('./models/Post'); // adjust if your path is different
+
+mongoose.connect('mongodb://localhost:27017/yourDatabaseName', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+const test = async () => {
+    try {
+        const p = new Post({ title: 'Test', content: 'This is a test' });
+        await p.save();
+        console.log('Saved post:', p);
+        mongoose.connection.close(); // close connection after done
+    } catch (err) {
+        console.error('Error saving post:', err);
+        mongoose.connection.close();
+    }
+};
+
+test();
